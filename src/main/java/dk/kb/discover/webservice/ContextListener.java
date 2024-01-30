9029c8b0ff925bf6148e301fd38a2ba8526bdbb1
Victor Harbo Johnston <vhol@kb.dk>
2024-01-30 12:44:41 +0100

    rename build-properties

--- src/main/java/dk/kb/discover/webservice/ContextListener.java
+++ src/main/java/dk/kb/discover/webservice/ContextListener.java
@@ -50,6 +50,7 @@ public class ContextListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
         //Workaround for logback problem. This should be called before any logging takes place
         initLogging();
+        BuildInfoManager.loadBuildInfo("ds-discover.build.properties");
 
     	
     	try {
