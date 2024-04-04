6abb93a878ce955d411f7b8c4f10bb4076289147
Toke Eskildsen <te@ekot.dk>
2024-04-04 21:22:29 +0200

    Add full implementation of SolrParamMerger

--- src/main/java/dk/kb/discover/util/SolrParamMerger.java
+++ src/main/java/dk/kb/discover/util/SolrParamMerger.java
@@ -21,6 +21,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
@@ -31,12 +34,20 @@ import java.util.stream.Collectors;
  * </ul>
  * The map is Solr-aware and treats {@code fq} as special-case, where default {@code fq}s are overridden by
  * user {@code fq}s and forced {@code fq}s merges with all other {@code fq}s.
+ * <p>
+ * Standard use case for the merger is to request an instance from {@link SolrParamMerger.Factory},
+ * add user-provided parameters and used the resulting param map for sending a request to Solr.
+ * <p>
+ * Note that any call to a getter or similar method automatically calls {@link #freeze()}, after which
+ * it is no longer possible to add more parameters. The will be reset if {@code #clear} is called.
  */
 public class SolrParamMerger extends LinkedHashMap<String, List<String>> {
     private static final Logger log = LoggerFactory.getLogger(SolrParamMerger.class);
 
     private final Map<String, List<String>> defaultParams;
     private final Map<String, List<String>> forcedParams;
+
+    private boolean frozen = false;
 
     private SolrParamMerger(Map<String, List<String>> defaultParams,
                             Map<String, List<String>> forcedParams) {
@@ -45,8 +56,202 @@ public class SolrParamMerger extends LinkedHashMap<String, List<String>> {
         this.forcedParams = forcedParams;
     }
 
-    
+    /**
+     * Convenience method that wraps the given value as a list.
+     * @param key key with which the specified value is to be associated.
+     * @param value value to be associated with the specified key.
+     * @return the previous value that was associated with the key.
+     */
+    public List<String> put(String key, String value) {
+        failIfFrozen();
+        return super.put(key, Collections.singletonList(value));
+    }
 
+    /**
+     * Convenience method for adding all single value parameters in the given {@code map}
+     * as lists of String.
+     * @param map mappings to added.
+     */
+    public void putAllSingle(Map<? extends String, ? extends String> map) {
+        failIfFrozen();
+        map.forEach(this::put);
+    }
+
+    /**
+     * If frozen, clear unfreezes the merger. Default values are not added after clear.
+     * @see #clear(boolean addDefaultValues)
+     */
+    @Override
+    public void clear() {
+        clear(false);
+    }
+
+    /**
+     * Clears the merger and adds default parameters.
+     * <p>
+     * If frozen, the merger is unfrozen.
+     * @param addDefaultValues if true, {@link #defaultParams} are added after clearing existing values.
+     */
+    public void clear(boolean addDefaultValues) {
+        frozen = false;
+        super.clear();
+        if (addDefaultValues) {
+            putAll(defaultParams);
+        }
+    }
+
+    @Override
+    public List<String> get(Object key) {
+        freeze();
+        return super.get(key);
+    }
+
+    @Override
+    public List<String> getOrDefault(Object key, List<String> defaultValue) {
+        freeze();
+        return super.getOrDefault(key, defaultValue);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        freeze();
+        return super.containsValue(value);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<String, List<String>> eldest) {
+        freeze();
+        return super.removeEldestEntry(eldest);
+    }
+
+    @Override
+    public Set<String> keySet() {
+        freeze();
+        return super.keySet();
+    }
+
+    @Override
+    public Collection<List<String>> values() {
+        freeze();
+        return super.values();
+    }
+
+    @Override
+    public Set<Map.Entry<String, List<String>>> entrySet() {
+        freeze();
+        return super.entrySet();
+    }
+
+    @Override
+    public void forEach(BiConsumer<? super String, ? super List<String>> action) {
+        freeze();
+        super.forEach(action);
+    }
+
+    @Override
+    public void replaceAll(BiFunction<? super String, ? super List<String>, ? extends List<String>> function) {
+        freeze();
+        super.replaceAll(function);
+    }
+
+    @Override
+    public List<String> put(String key, List<String> value) {
+        failIfFrozen();
+        return super.put(key, value);
+    }
+
+    @Override
+    public void putAll(Map<? extends String, ? extends List<String>> m) {
+        failIfFrozen();
+        super.putAll(m);
+    }
+
+    @Override
+    public List<String> putIfAbsent(String key, List<String> value) {
+        failIfFrozen();
+        return super.putIfAbsent(key, value);
+    }
+
+    @Override
+    public boolean remove(Object key, Object value) {
+        failIfFrozen();
+        return super.remove(key, value);
+    }
+
+    @Override
+    public boolean replace(String key, List<String> oldValue, List<String> newValue) {
+        failIfFrozen();
+        return super.replace(key, oldValue, newValue);
+    }
+
+    @Override
+    public List<String> replace(String key, List<String> value) {
+        failIfFrozen();
+        return super.replace(key, value);
+    }
+
+    @Override
+    public List<String> computeIfAbsent(String key, Function<? super String, ? extends List<String>> mappingFunction) {
+        failIfFrozen();
+        return super.computeIfAbsent(key, mappingFunction);
+    }
+
+    @Override
+    public List<String> computeIfPresent(String key, BiFunction<? super String, ? super List<String>, ? extends List<String>> remappingFunction) {
+        failIfFrozen();
+        return super.computeIfPresent(key, remappingFunction);
+    }
+
+    @Override
+    public List<String> compute(String key, BiFunction<? super String, ? super List<String>, ? extends List<String>> remappingFunction) {
+        failIfFrozen();
+        return super.compute(key, remappingFunction);
+    }
+
+    @Override
+    public List<String> merge(String key, List<String> value, BiFunction<? super List<String>, ? super List<String>, ? extends List<String>> remappingFunction) {
+        failIfFrozen();
+        return super.merge(key, value, remappingFunction);
+    }
+
+    /**
+     * Covenience method that throws an exception if the merger has been frozen.
+     * Called from mutators.
+     */
+    private void failIfFrozen() {
+        if (frozen) {
+            throw new IllegalStateException("Mutator method called on frozen merger." +
+                                            "All mutations must have finished before any getters are called");
+        }
+    }
+
+    /**
+     * Apply forced parameters, effectively finalizing the params for use.
+     * <p>
+     * After freezing it is no longer possible to add parameters.
+     */
+    private void freeze() {
+        if (frozen) {
+            return;
+        }
+        forcedParams.forEach((k, v) -> {
+            if ("fq".equals(k)) { // Forced fq is additive as Solr fq's always stack
+                if (containsKey("fq")) {
+                    List<String> fq = new ArrayList<>(get("fq")); // Ensure the list is mutable
+                    fq.addAll(v);
+                    put("fq", fq);
+                    return;
+                }
+            }
+            put(k, v);
+        });
+        frozen = true;
+    }
+
+
+    /**
+     * Cached default- and forced-params for cheap construction of {@link SolrParamMerger}s.
+     */
     public static class Factory {
         private final Map<String, List<String>> defaultParams;
         private final Map<String, List<String>> forcedParams;
@@ -75,11 +280,11 @@ public class SolrParamMerger extends LinkedHashMap<String, List<String>> {
          *     # params are overwritten
          *     forcedparams:
          * </pre>
-         * @param handler a Solr handler as specified in the configuration.
+         * @param handler a Solr handler as specified in the configuration, i.e. {@code select} or {@code mlt}.
          */
         public Factory(String handler) {
-            defaultParams = getParams("select.defaultparams");
-            forcedParams = getParams("select.forcedparams");
+            defaultParams = getParams(handler + ".defaultparams");
+            forcedParams = getParams(handler + ".forcedparams");
         }
 
         /**
@@ -111,28 +316,24 @@ public class SolrParamMerger extends LinkedHashMap<String, List<String>> {
                     .collect(Collectors.toMap(Pair::getKey, Pair::getValue));
         }
 
-        /**
-         * Create a new merger for the given handler. Handlers are defined in the application configuration.
-         * <p>
-         * When the addition of user provided parameters to the merger has finished, call {@link SolrParamMerger#applyForced}
-         * before extracting values from the merger.
-         * @return a new merger ready for receiving user provided parameters.
-         */
-        public static SolrParamMerger createMerger(String handler) {
-        }
     }
 
     /**
-     * Converts entries to pairs, with conversion of arrays to lists.
+     * Converts entries to pairs, with conversion of arrays and lists to String lists.
      * @param entry a Solr param entry.
      * @return a key-value pair with the entry data or null if {@link Map.Entry#getValue()} is an empty String array.
      */
     private static Pair<String, List<String>> toPair(Map.Entry<String, Object> entry) {
+        List<String> vals;
         if (entry.getValue() instanceof String[]) {
-            String[] vals = (String[])entry.getValue();
-            return vals.length == 0 ? null :
-                    new Pair<>(entry.getKey(), Arrays.asList(vals));
+            vals = Arrays.asList((String[])entry.getValue());
+        } else if (entry.getValue() instanceof List) {
+            vals = ((List<?>)entry.getValue()).stream()
+                    .map(Objects::toString)
+                    .collect(Collectors.toList());
+        } else {
+            vals = Collections.singletonList(Objects.toString(entry.getValue()));
         }
-        return new Pair<>(entry.getKey(), Collections.singletonList(Objects.toString(entry.getValue())));
+        return vals.isEmpty() ? null : new Pair<>(entry.getKey(), vals);
     }
 }
